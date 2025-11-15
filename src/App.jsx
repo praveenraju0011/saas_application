cda5ca418401265ddd970722d1d06d8f40a72c65
Praveen V <praveenraju0011@gamil.com>
2025-11-15 14:42:50 +0530

    added features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
+import Features from "./components/Features.jsx";
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
     </main>
   );
 };
