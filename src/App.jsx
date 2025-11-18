4a9a2c31e264f80f283b79971054afbe71931fc6
Praveen V <praveenraju0011@gamil.com>
2025-11-18 18:40:17 +0530

    added pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Features from "./components/Features.jsx";
+import Pricing from "./sections/Pricing.jsx";
+import Faq from "./sections/Faq.jsx";
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
       <Features />
+      <Pricing />
+      <Faq />
     </main>
   );
 };
